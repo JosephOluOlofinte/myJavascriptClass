9cf5d24b71dd8654407a239d9a97c70eeca88013
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2024-06-18 13:04:41 +0100

    first

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+// data types
+// primitive: number, string, null, boolean, BigInt, undefined
+// non-primitive: objects (objects; array)
+// primitve data types can only take one variable while non-promitive data types can take multiple variables
+
+// object = {key : value};
+const boy = {"maths": 20};
+console.log(boy)
+
+// json data []
+const jss1 = [
+
+    {"Bola": "Female"},
+    {"Fola": "Female"},
+    {"Kola": "Male"},
+    {"Ola": "Female"}
+];
+console.log(jss1);
+
+// array 
